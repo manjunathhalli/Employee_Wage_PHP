3007de055aecbc68e5d785bdaa00caf96d9a114e
manjunathhalli <manjunathhalli629@gmail.com>
2022-02-10 22:25:52 +0530

    Added:Check Employee Monthly Wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,32 +1,38 @@
 <?php
 class EmployeeWage
 {
-    function checkDailyWage()
+    private $IS_PRESENT = 1;
+    public $IS_FULL_TIMER = 1;
+    public $IS_PART_TIMER = 2;
+    public $wagePerHour = 20;
+    public $fullTimeEmpWorkingHour = 8;
+    public $partTimeEmpWorkingHour = 8;
+    public $workingDaysPerMonth = 20;
+    function calculateEmployeeWage()
     {
-        $IS_PRESENT = 1;
-        $IS_FULL_TIMER = 2;
-        $IS_PART_TIMER = 3;
-        $wagePerHour = 20;
-        $fullTimeEmpWorkingHour = 8;
-        $partTimeEmpWorkingHour = 8;
         $empCheck = rand(0, 1);
         echo "Employee Attendance : $empCheck";
-        $fullTimeEmpWagePerDay = $wagePerHour * $fullTimeEmpWorkingHour;
-        $partTimeEmpWagePerDay = $wagePerHour * $partTimeEmpWorkingHour;
-        switch ($empCheck == $IS_PRESENT) {
-            case $IS_FULL_TIMER:
-                echo "\nEmployee is Present";
-                echo "\nFull time Employee Daily Wage is : $fullTimeEmpWagePerDay";
+        switch ($empCheck == $this->IS_PRESENT) {
+            case $this->IS_FULL_TIMER:
+                echo "\n Employee is Present";
+                $fullTimeEmpWagePerDay = $this->wagePerHour * $this->fullTimeEmpWorkingHour;
+                echo "\n Full time Employee Daily Wage is : $fullTimeEmpWagePerDay";
+                $totalWagePerMonth = $this->workingDaysPerMonth * $fullTimeEmpWagePerDay;
+                echo "\n Full time Employee Monthly Wage is : $totalWagePerMonth";
                 break;
-            case $IS_PART_TIMER:
-                echo "\nEmployee is Present";
-                echo "\nPart time Employee Daily Wage is : $partTimeEmpWagePerDay";
+            case $this->IS_PART_TIMER:
+                echo "\n Employee is Present";
+                $partTimeEmpWagePerDay = $this->wagePerHour * $this->partTimeEmpWorkingHour;
+                echo "\n Part time Employee Daily Wage is : $partTimeEmpWagePerDay";
+                $totalWagePerMonth = $this->workingDaysPerMonth * $partTimeEmpWagePerDay;
+                echo "\n Part time Employee Monthly Wage is : $totalWagePerMonth";
                 break;
             default:
-                echo "\nEmployee is Absent";
+                echo "\n Employee is Absent";
+                echo "\n Employee Monthly Wage is : 0";
                 break;
         }
     }
 }
 $obj = new EmployeeWage();
-$obj->checkDailyWage();
+$obj->calculateEmployeeWage();
