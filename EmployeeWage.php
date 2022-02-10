6729a2946460d67f1275b66927aa392b2ed24fb0
manjunathhalli <manjunathhalli629@gmail.com>
2022-02-10 22:12:24 +0530

    Check Employee Daily Wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -4,10 +4,14 @@ class EmployeeWage
     function getEmpAttendance()
     {
         $IS_PRESENT = 1;
+        $wagePerHour = 20;
+        $workingHour = 8;
         $empCheck = rand(0, 1);
         echo "Employee Attendance : $empCheck";
+        $wagePerDay = $wagePerHour * $workingHour;
         if ($empCheck == $IS_PRESENT) {
             echo "\nEmployee is Present";
+            echo "\nEmployee Daily Wage is : $wagePerDay";
         } else {
             echo "\nEmployee is Absent";
         }
