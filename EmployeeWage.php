c4374aae9d69eb07cc9303b6b3246f9f8d971c53
manjunathhalli <manjunathhalli629@gmail.com>
2022-02-10 22:21:01 +0530

    Added: Use switch case

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,9 +1,11 @@
 <?php
 class EmployeeWage
 {
-    function getEmpAttendance()
+    function checkDailyWage()
     {
         $IS_PRESENT = 1;
+        $IS_FULL_TIMER = 2;
+        $IS_PART_TIMER = 3;
         $wagePerHour = 20;
         $fullTimeEmpWorkingHour = 8;
         $partTimeEmpWorkingHour = 8;
@@ -11,14 +13,20 @@ class EmployeeWage
         echo "Employee Attendance : $empCheck";
         $fullTimeEmpWagePerDay = $wagePerHour * $fullTimeEmpWorkingHour;
         $partTimeEmpWagePerDay = $wagePerHour * $partTimeEmpWorkingHour;
-        if ($empCheck == $IS_PRESENT) {
-            echo "\nEmployee is Present";
-            echo "\nFull time Employee Daily Wage is : $fullTimeEmpWagePerDay";
-            echo "\nPart time Employee Daily Wage is : $partTimeEmpWagePerDay";
-        } else {
-            echo "\nEmployee is Absent";
+        switch ($empCheck == $IS_PRESENT) {
+            case $IS_FULL_TIMER:
+                echo "\nEmployee is Present";
+                echo "\nFull time Employee Daily Wage is : $fullTimeEmpWagePerDay";
+                break;
+            case $IS_PART_TIMER:
+                echo "\nEmployee is Present";
+                echo "\nPart time Employee Daily Wage is : $partTimeEmpWagePerDay";
+                break;
+            default:
+                echo "\nEmployee is Absent";
+                break;
         }
     }
 }
 $obj = new EmployeeWage();
-$obj->getEmpAttendance();
+$obj->checkDailyWage();
