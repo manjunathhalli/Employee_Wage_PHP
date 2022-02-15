79bc899d42304f537d840e4ae4f65e4f9a9d7277
manjunathhalli <manjunathhalli629@gmail.com>
2022-02-15 22:36:13 +0530

    Added: Employee Wage for Multiple company

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -4,17 +4,14 @@ class EmployeeWage
     public $IS_FULL_TIME = 1;
     public $IS_PART_TIME = 2;
     public $IS_ABSENT = 0;
-    public $WAGE_PER_HR = 20;
-    public $MAX_WORKING_DAYS = 22;
-    public $MAX_HRS_IN_MONTH = 100;
     public $EMP_HRS;
 
-    function calculateEmployeeWage()
+    function calculateEmployeeWage($company, $WAGE_PER_HR, $MAX_WORKING_DAYS, $MAX_HRS_IN_MONTH)
     {
 
         $TOTAL_WORKING_DAYS = 0;
         $TOTAL_EMP_HRS = 0;
-        while ($TOTAL_WORKING_DAYS < $this->MAX_WORKING_DAYS && $TOTAL_EMP_HRS < $this->MAX_HRS_IN_MONTH) {
+        while ($TOTAL_WORKING_DAYS < $MAX_WORKING_DAYS && $TOTAL_EMP_HRS < $MAX_HRS_IN_MONTH) {
             $num = rand(0, 2);
             switch ($num) {
                 case 1:
@@ -31,10 +28,12 @@ class EmployeeWage
             $TOTAL_WORKING_DAYS++;
         }
 
-        $TOTAL_SALARY = $this->WAGE_PER_HR * $TOTAL_EMP_HRS;
-        echo "Total Salary :" . $TOTAL_SALARY;
+        $TOTAL_SALARY = $WAGE_PER_HR * $TOTAL_EMP_HRS;
+        echo "Total Employee wage for company " . $company . " is : " . $TOTAL_SALARY;
     }
 }
-
+echo "Welcome to Employee Wage Problem " . "\n";
 $wage = new EmployeeWage();
-$wage->calculateEmployeeWage();
+$wage->calculateEmployeeWage("D-Mart", 20, 22, 100);
+echo "\n";
+$wage->calculateEmployeeWage("Star Home", 15, 26, 120);
